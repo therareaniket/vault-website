78564392c83fa4fa98af704dd03f4971f99cb3da
therareaniket <aniketbhatti.rarepixelsdesign@gmail.com>
2025-11-25 17:20:09 +0530

    style 🎨: Added Timeline Animations

--- src/components/Securitypage/RegulatoryPreservation.tsx
+++ src/components/Securitypage/RegulatoryPreservation.tsx
@@ -1,6 +1,46 @@
 "use client"
 
+import { useEffect, useState } from "react";
 import Image from "next/image";
+import {  motion, } from 'framer-motion';
+
+const timelineAnimation = (typeof window !== "undefined" && window.innerWidth >= 1100) ?  {
+    initialLine: { width: '0%' },
+    animateLine: { width: '100%', transition: { duration: 4, delay: 0.2 }, },
+
+    initialDraft: { opacity: 0, y: 100, },
+    animateDraft: { opacity: 1, y: '-5px', transition: { duration: 0.5, delay: 0.3 },},
+
+    initialReview: { opacity: 0, y: -100, },
+    animateReview: { opacity: 1, y: '-5px', transition: { duration: 0.5, delay: 0.8 },},
+
+    initialApproved: { opacity: 0, y: 100, },
+    animateApproved: { opacity: 1, y: '-5px', transition: { duration: 0.5, delay: 1.6 },},
+
+    initialFinalize: { opacity: 0, y: -100, },
+    animateFinalize: { opacity: 1, y: '-5px', transition: { duration: 0.5, delay: 2.4 },},
+
+    initialArchived: { opacity: 0, y: 100, },
+    animateArchived: { opacity: 1, y: '-5px', transition: { duration: 0.5, delay: 3.2 },},
+} : {
+    initialLine: { width: '0%' },
+    animateLine: { width: '100%', },
+
+    initialDraft: { opacity: 1, y: 'auto', },
+    animateDraft: { opacity: 1, y: 'auto', },
+
+    initialReview: { opacity: 1, y: 'auto', },
+    animateReview: { opacity: 1, y: 'auto', },
+
+    initialApproved: { opacity: 1, y: 'auto', },
+    animateApproved: { opacity: 1, y: 'auto', },
+
+    initialFinalize: { opacity: 1, y: 'auto', },
+    animateFinalize: { opacity: 1, y: 'auto', },
+
+    initialArchived: { opacity: 1, y: 'auto', },
+    animateArchived: { opacity: 1, y: 'auto', },
+}
 
 export default function RegulatoryPreservation(){
     return (
@@ -15,7 +55,7 @@ export default function RegulatoryPreservation(){
 
                     <div className="prsrvtn-tl-procs">
                         <div className="process-main-wrapper">
-                            <div className="central-dot draft">
+                            <motion.div className="central-dot draft" variants={timelineAnimation} initial="initialDraft" whileInView="animateDraft" viewport={{ once: true, amount: 1 }}>
                                 <div className="process-info">
                                     <h3 className="h5">Draft</h3>
 
@@ -25,9 +65,9 @@ export default function RegulatoryPreservation(){
                                 <div className="process-images">
                                     <Image src="/images/securitycompliancepage/timeline-draft.svg" alt="timeline-draft" width={80} height={250} draggable={false} className="prsrvt-procs-img"></Image>
                                 </div>
-                            </div>
+                            </motion.div>
 
-                            <div className="central-dot in-review">
+                            <motion.div className="central-dot in-review" variants={timelineAnimation} initial="initialReview" whileInView="animateReview" viewport={{ once: true, amount: 1 }}>
                                 <div className="process-info alt-direction">
                                     <h3 className="h5">In Review</h3>
 
@@ -37,9 +77,9 @@ export default function RegulatoryPreservation(){
                                 <div className="process-images alt-direction">
                                     <Image src="/images/securitycompliancepage/timeline-in-review.svg" alt="timeline-draft" width={80} height={250} draggable={false} className="prsrvt-procs-img"></Image>
                                 </div>
-                            </div>
+                            </motion.div>
                             
-                            <div className="central-dot approved">
+                            <motion.div className="central-dot approved" variants={timelineAnimation} initial="initialApproved" whileInView="animateApproved" viewport={{ once: true, amount: 1 }}>
                                 <div className="process-info">
                                     <h3 className="h5">Approved</h3>
 
@@ -49,9 +89,9 @@ export default function RegulatoryPreservation(){
                                 <div className="process-images">
                                     <Image src="/images/securitycompliancepage/timeline-approved.svg" alt="timeline-approved" width={80} height={250} draggable={false} className="prsrvt-procs-img"></Image>
                                 </div>
-                            </div>
+                            </motion.div>
                             
-                            <div className="central-dot finalized">
+                            <motion.div className="central-dot finalized" variants={timelineAnimation} initial="initialFinalize" whileInView="animateFinalize" viewport={{ once: true, amount: 1 }}>
                                 <div className="process-info alt-direction">
                                     <h3 className="h5">Finalized</h3>
 
@@ -61,9 +101,9 @@ export default function RegulatoryPreservation(){
                                 <div className="process-images alt-direction">
                                     <Image src="/images/securitycompliancepage/timeline-finalize.svg" alt="timeline-draft" width={80} height={250} draggable={false} className="prsrvt-procs-img"></Image>
                                 </div>
-                            </div>
+                            </motion.div>
                             
-                            <div className="central-dot archived">
+                            <motion.div className="central-dot archived" variants={timelineAnimation} initial="initialArchived" whileInView="animateArchived" viewport={{ once: true, amount: 1 }}>
                                 <div className="process-info">
                                     <h3 className="h5">Archived (WORM)</h3>
 
@@ -73,8 +113,11 @@ export default function RegulatoryPreservation(){
                                 <div className="process-images">
                                     <Image src="/images/securitycompliancepage/timeline-archived.svg" alt="timeline-archived" width={80} height={250} draggable={false} className="prsrvt-procs-img"></Image>
                                 </div>
-                            </div>
+                            </motion.div>
+                            
+                            <motion.div className="central-line-animation" variants={timelineAnimation} initial="initialLine" whileInView="animateLine" viewport={{ once: true, amount: 1 }}></motion.div>
                         </div>
+
                     </div>
                 </div>
             </section>
